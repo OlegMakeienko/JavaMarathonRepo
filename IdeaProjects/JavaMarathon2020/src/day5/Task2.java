d2006bc7e2458b7c6bbab15830c7bbedf64823f0
OlegMakeienko <molva83@gmail.com>
2020-08-30 14:25:51 +0200

    add change to day5Task2. Use constructor instead set metod

--- IdeaProjects/JavaMarathon2020/src/day5/Task2.java
+++ IdeaProjects/JavaMarathon2020/src/day5/Task2.java
@@ -2,10 +2,8 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-        Motorbike motorbike1 = new Motorbike();
-        motorbike1.setModelOfBike("Kawasaki");
-        motorbike1.setColorOfBike("Black");
-        motorbike1.setYearOfIssueOfBike(2010);
+        Motorbike motorbike1 = new Motorbike("Kawasaki","Black",2010);
+
         System.out.println("Ваш мотоцикл: "+ motorbike1.getColorOfBike() + " " + motorbike1.getModelOfBike() + " " + motorbike1.getYearOfIssueOfBike() + " года выпуска");
 
     }
@@ -16,35 +14,21 @@ class Motorbike {
     private String color;
     private int yearOfIssue;
 
-    public void setModelOfBike(String model) {
-        if (model.isEmpty()) {
-            System.out.println("Введи марку авто");
-        } else {
-            this.model = model;
-        }
+    public Motorbike(String model, String color, int yearOfIssue) {
+        this.model = model;
+        this.color = color;
+        this.yearOfIssue = yearOfIssue;
     }
+
     public String getModelOfBike() {
         return model;
     }
 
-    public void setColorOfBike(String color) {
-        if (color.isEmpty()) {
-            System.out.println("Введи цвет авто");
-        } else {
-            this.color = color;
-        }
-    }
+
     public String getColorOfBike() {
         return color;
     }
 
-    public void setYearOfIssueOfBike(int yearOfIssue) {
-        if (yearOfIssue <= 0) {
-            System.out.println("Некорректный ввод года производства авто");
-        } else {
-            this.yearOfIssue = yearOfIssue;
-        }
-    }
     public int getYearOfIssueOfBike(){
         return yearOfIssue;
     }
